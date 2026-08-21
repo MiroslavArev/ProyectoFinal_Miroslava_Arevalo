71703ea69c25848ab87f9aa4f81f976e59fb5a4d
Miroslava Arevalo <miroslavastudent@gmail.com>
2026-08-20 22:14:23 -0500

    Ejercicio realizado del proyecto final

--- src/main/java/www/cibertec/Main.java
+++ src/main/java/www/cibertec/Main.java
@@ -4,14 +4,61 @@ package www.cibertec;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        //variables
+        String cliente = "Miroslava";
+        int codigoProducto = 1;
+        double precioProducto = 100.0;
+        boolean clienteFrecuente = true;
+        double subtotal = 0.0;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        //Calculo de producto
+        switch(codigoProducto){
+            case 1:
+                subtotal = precioProducto * 1;
+                break;
+            case 2:
+                subtotal = precioProducto * 2;
+                break;
+            case 3:
+                subtotal = precioProducto * 3;
+                break;
+            default:
+                System.out.println("Código de producto invalido");
+                return;
+        }
+        //Metodo del calculo de descuento
+        double descuento = calcularDescuento(subtotal);
+        double totalFinal = subtotal - descuento;
+
+        //Metodo para imprimir boleta
+        imprimirBoleta(cliente, clienteFrecuente, subtotal, descuento, totalFinal);
+
+    }
+
+    //calcular descuento
+    public static double calcularDescuento(double monto){
+        if(monto >=300.0){
+            return monto*0.30;
+        }else if(monto >=200.0){
+            return monto*0.20;
+        }else{
+            return 0.0;
         }
     }
+    public static void imprimirBoleta(String cliente, boolean clienteFrecuente, double subtotal, double descuento, double totalFinal){
+        System.out.println("RESUMEN DE COMPRA");
+        System.out.println("Cliente: " + cliente);
+        System.out.println("Cliente Frecuente: " + clienteFrecuente);
+        System.out.println("Subtotal: " + subtotal);
+        System.out.println("Descuento: " + descuento);
+        System.out.println("Total Final: " + totalFinal);
+
+        System.out.println("Pago en 3 cuotas");
+        for (int i = 1; i <= 3; i++) {
+            double cuota = totalFinal / 3;
+            System.out.println("Cuota " + i + ": " + cuota);
+        }
+    }
+
+
 }
